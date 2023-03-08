918ab7ab15bf1b0ed7ae308532e574df7730bce0
Mario Alberto Terán Acosta <marioteran1@hotmail.com>
2023-03-08 10:22:06 -0600

    App adaptation

--- aceptador.py
+++ aceptador.py
@@ -9,15 +9,11 @@ t_OTRO = r'.'
 def t_error(t):
     print('Error')
 
-def main():
+def lexer(cadena):
     aceptador = lex.lex()
-    cadena = input('Dame una cadena:')
     aceptador.input(cadena)
     el_token = aceptador.token()
     if el_token.type == 'LENG_1' or el_token.type == 'LENG_2':
-        print('Cadena aceptada')
+        return 'Cadena aceptada'
     else:
-        print('Cadena rechazada')
-
-if __name__ == '__main__':
-	main()
+        return 'Cadena rechazada'
--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import streamlit as st
+from aceptador import lexer
 
 st.title('Proyecto 2')
 st.markdown('Sea un lenguaje como en L1 = 1(0|1)+11 y sea un segundo lenguaje como en L2 = 11(1|0)∗1. Hagamos un aceptador de cadenas para el lenguaje L3 = L1|L2. El aceptador de cadenas debe pedir la cadena al usuario y responder si lo acepta o no lo hace.')
 
-st.header('Analizador léxico')
-cadena = st.text_input('')
+st.header('Lexer')
+cadena = st.text_input('Ingrese la cadena a analizar:')
 
 if st.button('Ingresar'):
-    resultado 
-          
-    st.text(f'El precio de la casa es de: ${result[0]}')
+    resultado = lexer(cadena)
+    st.text(f'Resultado: ${resultado}')
 
     
