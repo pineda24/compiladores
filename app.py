deb05364d04425887d95caf0c0b37d2a82ebebc8
Mario Alberto Terán Acosta <marioteran1@hotmail.com>
2023-03-08 10:23:32 -0600

    App adaptation

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from aceptador import lexer
 
 st.title('Proyecto 2')
-st.markdown('Sea un lenguaje como en L1 = 1(0|1)+11 y sea un segundo lenguaje como en L2 = 11(1|0)∗1. Hagamos un aceptador de cadenas para el lenguaje L3 = L1|L2. El aceptador de cadenas debe pedir la cadena al usuario y responder si lo acepta o no lo hace.')
+st.markdown('Sea un lenguaje como en L1 = 1(0|1)+11 y sea un segundo lenguaje como en L2 = 11(1|0)*1. Hagamos un aceptador de cadenas para el lenguaje L3 = L1|L2. El aceptador de cadenas debe pedir la cadena al usuario y responder si lo acepta o no lo hace.')
 
 st.header('Lexer')
 cadena = st.text_input('Ingrese la cadena a analizar:')
